37071f3b3f43326a13756cc6e542a6e70b0b7b12
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-05-21 20:10:15 -0600

    Up

---
+++ groceryManagement/groceryManagement/Vendor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groceryManagement
+{
+    public class Vendor
+    {
+        //Attributes
+        private string name;
+
+        private string description;
+
+        private int vendorID;
+
+        
+        //Constructor
+        public Vendor()
+        {
+
+        }
+
+        public Vendor(string name, string description, int vendorID)
+        {
+            this.name = name;
+            this.description = description;
+            this.vendorID = vendorID;
+        }
+
+
+
+        //----------------------------------------------------------------------//
+        //Getters and setters for private attributes
+
+        //Vendor name
+        public  string VendorName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        //Vendor description
+
+        public string VendorDescription
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        //Vendor ID
+        public int VendorID
+        {
+            get { return vendorID; }
+            set { vendorID = value; }
+        }
+
+        //----------------------------------------------------------------------//
+        //Methods
+
+        //Set new vendor
+        public void addNewVendor(string vendorName, string vendorDesc, int vendorID)
+        {
+
+        }
+
+        //Get all vendor
+        public List<int> getAllVendor()
+        {
+            return new List<int>();
+        }
+
+        //Check specific Vendor
+        public int getVendor(int vendorID)
+        {
+            return 0;
+        }
+    }
+}
---
+++ groceryManagement/groceryManagement/sqlConnect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace groceryManagement
+{
+    public abstract class sqlConnect
+    {
+
+        protected string connectionString = "Server=localhost;Database=grocerymanagement;User Id=root;Password=password;";
+
+        public string GetConnectionString()
+        {
+            return connectionString;
+        }
+
+    }
+}
