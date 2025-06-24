a759613a41f22ea4a2bdfac24f9d79fd031e3710
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-06-24 15:52:54 -0600

    d

--- groceryManagement/groceryManagement/Program.cs
+++ groceryManagement/groceryManagement/Program.cs
@@ -6,5 +6,40 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        class Menu
+        {
+            //options
+            public enum Option
+            {
+                None,
+                Purchasing = 1,
+                Recieving = 2,
+                Inventory = 3,
+                exit = 4,
+
+            }
+
+            public void DisplayMenu()
+            {
+                Console.WriteLine($"\nGrocery Mangement");
+                Console.WriteLine($"\n");
+            }
+        }
+
+        class Purchasing
+        {
+
+        }
+
+        class Recieving
+        {
+
+        }
+
+        class Inventory
+        {
+
+        }
     }
 }
